7741017a5aaebbc430426725d434759853922264
None <tavusB@bastion-fuchicorp-com.us-central1-a.c.geometric-sled-282503.internal>
2020-09-10 19:39:27 +0000

    commit "test"

--- deployments/terraform/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "gcs" {
-    bucket  = "tavus_bucket"
-    prefix  = "stage/hello-world"
-    project = "fuchi-287613"
-  }
-}
--- deployments/terraform/set-env.sh
+++ deployments/terraform/set-env.sh
@@ -1,12 +1,23 @@
 #!/usr/bin/env bash
+red=$(tput setaf 196)
+reset=$(tput sgr0)
+# condition in [] will check variable GIT_TOKEN is empty or no, if "empty" return 1 
 if [ -z "$GIT_TOKEN" ]
 then
   echo """
   Script needs github token to get private repos.
-  Github <GIT_TOKEN> not found."""
-  return 1 
+  Github <GIT_TOKEN> not found.
+    run command  <$red export GIT_TOKEN='your_token'$reset>
+   """
+  #return 1
+# if we have variable GIT_TOKEN, condition in [] will check is it valid token or no.
+elif  [ "$(curl -s -H "Authorization: token $GIT_TOKEN" 'https://api.github.com' -I | awk '{print $2}' | head -1)" != "200"  ]
+then 
+   echo "GIT_TOKEN is not valid"
+elif  [ "$(curl -s -H "Authorization: token $GIT_TOKEN" 'https://api.github.com' -I | awk '{print $2}' | head -1)" == "200"  ]
+then
+    wget --quiet "--header=Authorization: token $GIT_TOKEN" "https://raw.githubusercontent.com/fuchicorp/common_scripts/master/set-environments/terraform/google-set-env.sh" \
+      -O set-env >/dev/null
+    source 'set-env' "$@"
+    rm -rf "set-env"
 fi
-wget --quiet "--header=Authorization: token $GIT_ADMIN_TOKEN" "https://raw.githubusercontent.com/fuchicorp/common_scripts/master/set-environments/terraform/google-set-env.sh" \
-  -O set-env >/dev/null
-source 'set-env' "$@"
-rm -rf "set-env"
